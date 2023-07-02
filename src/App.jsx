8ca8cd7769f5b844600428e7f0cf0d6663107430
hasankarim18 <hallysust@gmail.com>
2023-07-02 16:33:27 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div className="container mx-auto" >
+        <h3 className="text-3xl">React, Vite, Tailwind Stater template</h3>
+     </div>
+    </>
+  )
+}
+
+export default App
